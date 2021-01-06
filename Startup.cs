450bdbc113b8540ce1a222002cce1c0865bc7458
rhkean <rhkean@gmail.com>
2021-01-05 22:17:40 -0500

    added a simple CRUD page from here: https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-javascript?view=aspnetcore-5.0

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,9 @@ namespace linkmir
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
